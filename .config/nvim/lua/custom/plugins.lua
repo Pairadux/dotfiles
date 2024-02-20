fcf3e19caff75752ce8ad310d352794f98bfe792
AustinCGause <austincgause@gmail.com>
2024-02-20 13:41:42 -0500

    added auto-save.nvim

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -65,6 +65,16 @@ local plugins = {
     end,
   },
 
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+  },
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
