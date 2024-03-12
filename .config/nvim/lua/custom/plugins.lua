4a0c34d3c41a235b7ccf8054b400719c6b43887e
AustinCGause <austincgause@gmail.com>
2024-03-11 20:36:20 -0400

    updated auto-save nvim

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -62,7 +62,15 @@ local plugins = {
 		"okuuva/auto-save.nvim",
 		cmd = "ASToggle", -- optional for lazy loading on command
 		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
-		opts = {},
+		opts = {
+            trigger_events = {
+                immediate_save = {
+                    { "BufLeave", "FocusLost" }
+                },
+                defer_save = {},
+                cancel_defer_save = {},
+            }
+        },
 	},
 
 	{
