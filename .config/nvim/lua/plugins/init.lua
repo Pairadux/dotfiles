c263d63e72c1fe5da08ea41805a0bf15ce15cb5b
AustinCGause <austincgause@gmail.com>
2024-03-16 00:51:38 -0400

    added notify and changed noice configs

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -51,13 +51,58 @@ return {
 	},
 
 	{
+		"rcarriga/nvim-notify",
+		config = function()
+			require("notify").setup({
+				background_colour = "#000000",
+				render = "minimal",
+			})
+		end,
+	},
+
+	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {
-		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
 		},
+		config = function()
+			require("noice").setup({
+				lsp = {
+					signature = {
+						enabled = false,
+					},
+					hover = {
+						enabled = false,
+					},
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+						["vim.lsp.util.stylize_markdown"] = false,
+						["cmp.entry.get_documentation"] = false, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				progress = {
+					enabled = false,
+				},
+				signature = {
+					enabled = false,
+				},
+				presets = {
+					-- you can enable a preset by setting it to true, or a table that will override the preset config
+					-- you can also add custom presets that you can enable/disable with enabled=true
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+        init = function()
+            vim.g.lsp_handlers_enabled = false
+        end,
 	},
 
 	{
