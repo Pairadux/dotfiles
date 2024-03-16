e5c5a8509807f58af0c8d4722e63a04c0f7e1632
AustinCGause <austincgause@gmail.com>
2024-03-16 01:16:44 -0400

    updated harpoon config

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local harpoon = require("harpoon")
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
@@ -11,3 +12,15 @@ map("n", "<leader>fm", function()
 end, { desc = "File Format with conform" })
 
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
+
+map("n", "<leader>a", function() harpoon:list():append() end, { desc = "Add line under cursor to Harpoon"})
+map("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggle Harpoon Quick Menu"})
+
+map("n", "<C->", function() harpoon:list():select(1) end, { desc = "Quick Select First Harpoon Option"})
+map("n", "<C->", function() harpoon:list():select(2) end, { desc = "Quick Select Second Harpoon Option"})
+map("n", "<C->", function() harpoon:list():select(3) end, { desc = "Quick Select Third Harpoon Option"})
+map("n", "<C->", function() harpoon:list():select(4) end, { desc = "Quick Select Fourth Harpoon Option"})
+
+-- Toggle previous & next buffers stored within Harpoon list
+map("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Toggle Previous Buffer in Harpoon List"})
+map("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Toggle Next Buffer in Harpoon List"})
