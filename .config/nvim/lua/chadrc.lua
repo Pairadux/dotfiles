47bfac1103e498632fb5430bf5fd4edda15c914e
AustinCGause <austincgause@gmail.com>
2024-03-15 23:55:35 -0400

    added dashboard nvim

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -3,19 +3,15 @@ local M = {}
 M.ui = {
 	theme = "tokyonight",
 
-    transparency="true",
+	transparency = "true",
 
-    statusline = {
-        theme = "vscode_colored",
-    },
+	statusline = {
+		theme = "vscode_colored",
+	},
 
-    nvdash = {
-        load_on_startup = true,
-    },
-
-    term = {
-        sizes = { sp = 0.3, vsp = 0.3 },
-    },
+	term = {
+		sizes = { sp = 0.3, vsp = 0.3 },
+	},
 }
 
 M.plugins = "plugins"
