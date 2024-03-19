1846c839715e667c4680d1ea40e6ad465f5be5bd
AustinCGause <austincgause@gmail.com>
2024-03-19 01:07:22 -0400

    added event to todo-comments

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -160,6 +160,7 @@ return {
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
+        event = "BufEnter",
 		opts = {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
