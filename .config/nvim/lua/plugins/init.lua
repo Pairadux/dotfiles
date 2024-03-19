f0eb16db7bd6eb8aff4d9012308343f15a1fee01
AustinCGause <austincgause@gmail.com>
2024-03-19 00:58:06 -0400

    added todo-comments nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -157,6 +157,16 @@ return {
 		end,
 	},
 
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+
 	-- {
 	--     "christoomey/vim-tmux-navigator",
 	--     lazy = false,
