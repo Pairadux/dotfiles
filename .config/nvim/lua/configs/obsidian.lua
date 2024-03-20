499efb3271e6f882303dd5872115e18e968bf55f
AustinCGause <austincgause@gmail.com>
2024-03-19 22:27:41 -0400

    added obsidian.nvim

---
+++ .config/nvim/lua/configs/obsidian.lua
@@ -0,0 +1,15 @@
+local options = {
+    dir = "~/Documents/Brain 2.0",
+    daily_notes = {
+        folder = "000 - Zettelkasten/050 - Daily Notes",
+        date_format = "%Y-%m-%d", -- CHANGE
+        template = nil -- CHANGE
+    },
+    mappings = {},
+    preferred_link_style = "wiki",
+    templates = {
+        subdir = "900 - Templates"
+    }
+}
+
+return options
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -169,6 +169,27 @@ return {
 	},
 
 	{
+		"epwalsh/obsidian.nvim",
+		version = "*",
+		lazy = true,
+		event = {
+		  -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		  -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+		  "BufReadPre " .. vim.fn.expand "~" .. "/Documents/Brain 2.0/**.md",
+		  "BufNewFile " .. vim.fn.expand "~" .. "/Documents/Brain 2.0/**.md",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+            "nvim-telescope/telescope.nvim",
+            "nvim-treesitter",
+		},
+        opts = function ()
+            require("configs.obsidian")
+        end
+	},
+
+	{
 		"christoomey/vim-tmux-navigator",
 		lazy = false,
 	},
