edc5bd1c878839cca79b9d118dbf8f36382fc404
AustinCGause <austincgause@gmail.com>
2024-03-26 22:24:33 -0400

    gave border to floating diagnostic

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -10,7 +10,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 
 -- Format with conform
 map("n", "<leader>fm", function()
-  require("conform").format()
+	require("conform").format()
 end, { desc = "File Format with conform" })
 
 -- Exit insert mode
@@ -19,29 +19,45 @@ map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
 -- TERMS
 -- Creating new toggleable terms
 map({ "n", "t" }, "<A-8>", function()
-    require("nvchad.term").toggle { pos = "float", id = "1" }
+	require("nvchad.term").toggle({ pos = "float", id = "1" })
 end, { desc = "Terminal Toggle Floating Term 1" })
 map({ "n", "t" }, "<A-9>", function()
-    require("nvchad.term").toggle { pos = "float", id = "2" }
+	require("nvchad.term").toggle({ pos = "float", id = "2" })
 end, { desc = "Terminal Toggle Floating Term 2" })
 map({ "n", "t" }, "<A-0>", function()
-    require("nvchad.term").toggle { pos = "float", id = "3" }
+	require("nvchad.term").toggle({ pos = "float", id = "3" })
 end, { desc = "Terminal Toggle Floating Term 3" })
 
 -- HARPOON
 -- Harpoon Main Options
-map("n", "<leader>a", function() harpoon:list():append() end, { desc = "Add line under cursor to Harpoon"})
-map("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggle Harpoon Quick Menu"})
+map("n", "<leader>a", function()
+	harpoon:list():append()
+end, { desc = "Add line under cursor to Harpoon" })
+map("n", "<C-e>", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "Toggle Harpoon Quick Menu" })
 
 -- Harpoon Quick Select Option
-map("n", "<C->", function() harpoon:list():select(1) end, { desc = "Quick Select First Harpoon Option"})
-map("n", "<C->", function() harpoon:list():select(2) end, { desc = "Quick Select Second Harpoon Option"})
-map("n", "<C->", function() harpoon:list():select(3) end, { desc = "Quick Select Third Harpoon Option"})
-map("n", "<C->", function() harpoon:list():select(4) end, { desc = "Quick Select Fourth Harpoon Option"})
+map("n", "<C->", function()
+	harpoon:list():select(1)
+end, { desc = "Quick Select First Harpoon Option" })
+map("n", "<C->", function()
+	harpoon:list():select(2)
+end, { desc = "Quick Select Second Harpoon Option" })
+map("n", "<C->", function()
+	harpoon:list():select(3)
+end, { desc = "Quick Select Third Harpoon Option" })
+map("n", "<C->", function()
+	harpoon:list():select(4)
+end, { desc = "Quick Select Fourth Harpoon Option" })
 
 -- Toggle previous & next buffers stored within Harpoon list
-map("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Toggle Previous Buffer in Harpoon List"})
-map("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Toggle Next Buffer in Harpoon List"})
+map("n", "<C-S-P>", function()
+	harpoon:list():prev()
+end, { desc = "Toggle Previous Buffer in Harpoon List" })
+map("n", "<C-S-N>", function()
+	harpoon:list():next()
+end, { desc = "Toggle Next Buffer in Harpoon List" })
 
 -- TODO-COMMENTS
 map("n", "<leader>tt", "<cmd>Telescope todo-comments<CR>", { desc = "Telescope Todo Comments" })
@@ -57,3 +73,8 @@ map("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Open Obsidian Q
 
 -- Telescope
 map("n", "<leader>tp", "<cmd>Telescope project<CR>", { desc = "Telescope Projects" })
+
+-- Diagnostics
+map("n", "<leader>lf", function()
+	vim.diagnostic.open_float(nil, { border = "single" })
+end, { desc = "Lsp floating diagnostics" })
