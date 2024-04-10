8051bb4d1c5b75612c2f12cb20fbd9b3e58fa062
AustinCGause <austincgause@gmail.com>
2024-04-09 23:23:05 -0400

    style: formatted file

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,19 +4,19 @@ local on_attach = configs.on_attach
 local on_init = configs.on_init
 local capabilities = configs.capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "clangd", "svelte", "tailwindcss", "marksman", "bashls", }
+local lspconfig = require("lspconfig")
+local servers = { "html", "cssls", "tsserver", "clangd", "svelte", "tailwindcss", "marksman", "bashls" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_init = on_init,
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_init = on_init,
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
--- Without the loop, you would have to manually set up each LSP 
--- 
+-- Without the loop, you would have to manually set up each LSP
+--
 -- lspconfig.html.setup {
 --   on_attach = on_attach,
 --   capabilities = capabilities,
