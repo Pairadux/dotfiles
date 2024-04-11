6dc17e6866b19b45cbde6fb3d38b452019e7da30
AustinCGause <austincgause@gmail.com>
2024-04-11 00:26:31 -0400

    Refactor: moved obsidian opts to its own config folder
    Obsidian configuration section was getting too large, needed to be moved
    to another file for maintainability

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -234,24 +234,9 @@ return {
 			"nvim-telescope/telescope.nvim",
 			"nvim-treesitter",
 		},
-		opts = {
-			workspaces = {
-				{
-					name = "Brain 2.0",
-					path = "~/Documents/Brain 2.0",
-				},
-			},
-			daily_notes = {
-				folder = "000 - Zettelkasten/050 - Daily Notes",
-				date_format = "%Y-%m-%d", -- CHANGE
-				template = nil, -- CHANGE
-			},
-			mappings = {},
-			preferred_link_style = "wiki",
-			templates = {
-				subdir = "900 - Templates",
-			},
-		},
+		config = function()
+			require("configs.obsidian")
+		end,
 	},
 
 	{
