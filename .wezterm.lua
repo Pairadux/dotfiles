3c4615a21f6069da5ccf99e56da1daa54106e872
AustinCGause <austincgause@gmail.com>
2024-04-30 11:44:50 -0400

    Config(wezterm): updated wezterm to open zellij on start

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,8 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+-- config.default_prog = { 'zellij' }
+
 -- For example, changing the color scheme:
 config.window_decorations = "RESIZE"
 config.color_scheme = "Tokyo Night"
