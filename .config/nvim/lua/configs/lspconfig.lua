2c6d153ebd2ae029ddbf556eb4d67b9986899d5c
Colt <austincgause@gmail.com>
2024-05-14 21:26:08 -0400

    Config(nvim:lspconfig): updated servers

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,25 +1,78 @@
 local configs = require("nvchad.configs.lspconfig")
+local lspconfig = require("lspconfig")
 
 local servers = {
-    html = {},
-    cssls = {},
-    tsserver = {},
-    clangd = {},
-    svelte = {},
-    marksman = {},
-    bashls = {},
-    rust_analyzer = {},
+	html = {},
+	cssls = {},
+	tsserver = {},
+	clangd = {},
+	svelte = {},
+	marksman = {},
+	bashls = {},
+	rust_analyzer = {},
+	-- basedpyright = {
+	-- 	settings = {
+	-- 		pyright = {
+	-- 			disableOrganizeImports = true,
+	-- 		},
+	-- 		python = {
+	-- 			analysis = {
+	-- 				ignore = { "*" },
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
+	tailwindcss = {
+		filetypes = { "html", "css", "svelte", "scss", "javascript", "typescript", "vue" },
+	},
+    pylsp = {
+        settings = {
+            pylsp = {
+                plugins = {
+                    pycodestyle = {
+                        enabled = false
+                    },
+                    flake8 = {
+                        enabled = false
+                    },
+                    pyflakes = {
+                        enabled = false
+                    },
+                    pylint = {
+                        enabled = false
+                    },
+                    rope = {
+                        enabled = false
+                    },
+                    mccabe = {
+                        enabled = false
+                    },
+                }
+            }
+        }
+    },
     ruff_lsp = {},
-    pyright = {},
-    tailwindcss = {
-        filetypes = { "html", "css", "svelte", "scss", "javascript", "typescript", "vue" }
-    }
 }
 
 for name, opts in pairs(servers) do
-  opts.on_init = configs.on_init
-  opts.on_attach = configs.on_attach
-  opts.capabilities = configs.capabilities
+	opts.on_init = configs.on_init
+	opts.on_attach = configs.on_attach
+	opts.capabilities = configs.capabilities
 
-  require("lspconfig")[name].setup(opts)
+	lspconfig[name].setup(opts)
 end
+
+-- local nvchad_on_attach = configs.on_attach
+-- local custom_on_attach = function(client, bufnr)
+-- 	nvchad_on_attach()
+-- 	if client.name == "ruff_lsp" then
+-- 		-- Disable hover in favor of Pyright
+-- 		client.server_capabilities.hoverProvider = false
+-- 	end
+-- end
+--
+-- lspconfig.ruff_lsp.setup({
+-- 	on_init = configs.on_init,
+-- 	on_attach = custom_on_attach,
+-- 	capabilities = configs.capabilities,
+-- })
