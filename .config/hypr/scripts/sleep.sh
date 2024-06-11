d2cec1a899b67141c23277ae87f87ba4d64d0618
AustinCGause <austincgause@gmail.com>
2024-06-11 00:37:20 -0400

    Config(hyprland): altered autosleep conditions

--- .config/hypr/scripts/sleep.sh
+++ .config/hypr/scripts/sleep.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-swayidle -w timeout 300 'hyprlock' timeout 600 'systemctl suspend' before-sleep 'hyprctl dispatch dpms off' &
+# swayidle -w timeout 300 'hyprlock' \
+#             timeout 600 'hyprctl dispatch dpms off' \
+#             resume 'hyprctl dispatch dpms on' \
+#             timeout 900 'systemctl suspend' \
+#             before-sleep 'hyprlock' &
+
+swayidle -w timeout 300 'hyprlock' \
+            timeout 600 'systemctl suspend' \
+            before-sleep 'hyprlock' &
