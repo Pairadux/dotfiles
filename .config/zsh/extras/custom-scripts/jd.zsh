6edafb8eaf2c921d1462574b23a650f0fad3b156
Colt <austincgause@gmail.com>
2024-09-13 01:22:22 -0400

    Config(zsh:jd): updated jd script to include fzf

--- .config/zsh/extras/custom-scripts/jd.zsh
+++ .config/zsh/extras/custom-scripts/jd.zsh
@@ -1,31 +1,31 @@
 #!/usr/bin/env zsh
 
+# JD script not using fzf
+# jd() {
+#     local dir
+#     dir=$(fd -t d "^$1.*$" ~/Documents/SyncFolder -d 3 | head -n 1)
+#     if [ -n "$dir" ]; then
+#         builtin cd "$dir" 2>/dev/null || echo "Cannot change to directory: $dir"
+#     else
+#         echo "No matching directory found for $1"
+#     fi
+# }
+
+# JD script using fzf
 jd() {
-    local dir
-    dir=$(fd -t d "^$1.*$" ~/Documents/SyncFolder -d 3 | head -n 1)
-    if [ -n "$dir" ]; then
-        builtin cd "$dir" 2>/dev/null || echo "Cannot change to directory: $dir"
+    local selected_dir
+
+    if [ "$#" -eq 0 ]; then
+        # If no arguments, show all directories
+        selected_dir=$(fd -t d . ~/Documents/SyncFolder -d 3 | fzf --height 50% --reverse)
     else
-        echo "No matching directory found for $1"
+        # If argument provided, use it as initial query
+        selected_dir=$(fd -t d . ~/Documents/SyncFolder -d 3 | fzf --height 50% --reverse -q "$1")
+    fi
+
+    if [ -n "$selected_dir" ]; then
+        cd "$selected_dir"
+    else
+        echo "No directory selected."
     fi
 }
-
-# _completejd() {
-#     local prefix="$1"
-#     local completions
-#
-#     if [[ "$prefix" =~ ^[0-9]{2}$ ]]; then
-#         # If two digits are entered, complete with matching xx.yy patterns
-#         completions=($(fd -t d "^$prefix\.[0-9]{2}" ~/Documents/SyncFolder -d 3 | sed -E 's|.*/([0-9]{2}\.[0-9]{2}).*|\1|' | sort -u))
-#     elif [[ "$prefix" =~ ^[0-9]$ ]]; then
-#         # If one digit is entered, complete with matching xy patterns where x is the entered digit
-#         completions=($(fd -t d "^$prefix[0-9]-" ~/Documents/SyncFolder -d 2 | sed -E 's|.*/([0-9]{2})-.*|\1|' | sort -u))
-#     else
-#         # If no digits are entered, show all top-level categories
-#         completions=($(fd -t d "^[0-9]{2}-" ~/Documents/SyncFolder -d 2 | sed -E 's|.*/([0-9]{2})-.*|\1|' | sort -u))
-#     fi
-#
-#     reply=("${completions[@]}")
-# }
-#
-# compctl -K _completejd jd
