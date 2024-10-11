fd729f33ee1c53b03e3641eb8c5a8fd8de4dd92b
AustinCGause <austincgause@gmail.com>
2024-10-11 00:55:30 -0400

    Config(nvim:mappings): removed harpoon mappings, changed telescope mappings, and removed some default nvchad mappings

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,42 +1,43 @@
 require("nvchad.mappings")
 
+
 -- Global Declarations
 local map = vim.keymap.set
-local harpoon = require("harpoon")
+local nomap = vim.keymap.del
+-- local harpoon = require("harpoon")
 
--- -- Enter Command Mode
--- map("n", ";", ":")
---
--- -- Next ftFT
--- map("n", ":", ";")
+-- Remove some default nvchad mappings
+nomap("n", "<leader>th")
+nomap("n", "<leader>n")
+nomap("n", "<leader>rn")
+nomap("n", "<leader>ch")
+nomap("n", "<leader>b")
+nomap("n", "<leader>e")
+nomap("n", "<leader>v")
+nomap("n", "<leader>h")
 
--- close nvim with q
+-- close nvim with q, different action based on buffer type
 map("n", "q", function()
-    if vim.bo.modifiable then
-        vim.cmd("wq")
-    else
-        vim.cmd("q")
-    end
+	if vim.bo.modifiable then
+		vim.cmd("wq")
+	else
+		vim.cmd("q")
+	end
 end, { noremap = true, silent = true })
 
 -- Reload nvim
 map("n", "<leader>rn", function()
-    require("nvchad.utils").reload()
+	require("nvchad.utils").reload()
 end, { desc = "Reload nvim" })
-
--- Format with conform
-map("n", "<leader>fm", function()
-	require("conform").format()
-end, { desc = "File Format with conform" })
 
 -- Exit insert mode
 map("i", "jk", "<ESC>")
 
 -- Insert Line Break Sequence
-map("n", "<leader>ib", "80i#<ESC>", { desc = "Insert 80 #'s (Line Break)" })
+map("n", "<leader>ib", "80i#<ESC>", { noremap = true, silent = true, desc = "Insert 80 #'s (Line Break)" })
 
 -- Insert Config Section Title Box
-map("n", "<leader>it", "O# *-*<CR># | |<CR># *-*<ESC>", { desc = "Insert Config Section Title Box" })
+map("n", "<leader>it", "O# *-*<CR># | |<CR># *-*<ESC>", { noremap = true, silent = true, desc = "Insert Title Box" })
 
 -- TERMS
 -- Creating new toggleable terms
@@ -50,39 +51,37 @@ map({ "n", "t" }, "<A-0>", function()
 	require("nvchad.term").toggle({ pos = "float", id = "3" })
 end, { desc = "Terminal Toggle Floating Term 3" })
 
--- HARPOON
--- Harpoon Main Options
-map("n", "<leader>a", function()
-	harpoon:list():add()
-end, { desc = "Add line under cursor to Harpoon" })
-map("n", "<C-e>", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
-end, { desc = "Toggle Harpoon Quick Menu" })
+-- -- HARPOON
+-- -- Harpoon Main Options
+-- map("n", "<leader>a", function()
+-- 	harpoon:list():add()
+-- end, { desc = "Add line under cursor to Harpoon" })
+-- map("n", "<C-e>", function()
+-- 	harpoon.ui:toggle_quick_menu(harpoon:list())
+-- end, { desc = "Toggle Harpoon Quick Menu" })
+--
+-- -- Harpoon Quick Select Option
+-- map("n", "<leader>1", function()
+-- 	harpoon:list():select(1)
+-- end, { desc = "Quick Select First Harpoon Option" })
+-- map("n", "<leader>2", function()
+-- 	harpoon:list():select(2)
+-- end, { desc = "Quick Select Second Harpoon Option" })
+-- map("n", "<leader>3", function()
+-- 	harpoon:list():select(3)
+-- end, { desc = "Quick Select Third Harpoon Option" })
+-- map("n", "<leader>4", function()
+-- 	harpoon:list():select(4)
+-- end, { desc = "Quick Select Fourth Harpoon Option" })
+--
+-- -- Toggle previous & next buffers stored within Harpoon list
+-- map("n", "<C-S-P>", function()
+-- 	harpoon:list():prev()
+-- end, { desc = "Toggle Previous Buffer in Harpoon List" })
+-- map("n", "<C-S-N>", function()
+-- 	harpoon:list():next()
+-- end, { desc = "Toggle Next Buffer in Harpoon List" })
 
--- Harpoon Quick Select Option
-map("n", "<leader>1", function()
-	harpoon:list():select(1)
-end, { desc = "Quick Select First Harpoon Option" })
-map("n", "<leader>2", function()
-	harpoon:list():select(2)
-end, { desc = "Quick Select Second Harpoon Option" })
-map("n", "<leader>3", function()
-	harpoon:list():select(3)
-end, { desc = "Quick Select Third Harpoon Option" })
-map("n", "<leader>4", function()
-	harpoon:list():select(4)
-end, { desc = "Quick Select Fourth Harpoon Option" })
-
--- Toggle previous & next buffers stored within Harpoon list
-map("n", "<C-S-P>", function()
-	harpoon:list():prev()
-end, { desc = "Toggle Previous Buffer in Harpoon List" })
-map("n", "<C-S-N>", function()
-	harpoon:list():next()
-end, { desc = "Toggle Next Buffer in Harpoon List" })
-
--- TODO-COMMENTS
-map("n", "<leader>tt", "<cmd>Telescope todo-comments<CR>", { desc = "Telescope Todo Comments" })
 
 -- TMUX NAVIGATOR
 map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "Tmux Window Left" })
@@ -102,7 +101,8 @@ map("n", "<leader>ot", "<cmd>ObsidianTags<CR>", { desc = "Obsidian Tags" })
 map("v", "<leader>oe", "<cmd>ObsidianExtractNote<CR>", { desc = "Obsidian Extract Note" })
 
 -- Telescope
-map("n", "<leader>tp", "<cmd>Telescope project<CR>", { desc = "Telescope Projects" })
+map("n", "<leader>fp", "<cmd>Telescope project<CR>", { desc = "Telescope projects" })
+map("n", "<leader>ft", "<cmd>Telescope todo-comments<CR>", { desc = "Telescope todo comments" })
 
 -- Toggleable LazyGit Term
 map({ "n", "t" }, "<A-l>", function()
@@ -121,7 +121,7 @@ map({ "n", "t" }, "<leader>rc", function()
 			local ft_cmds = {
 				python = "python3 " .. file,
 				cpp = "clear && g++ -o out " .. file .. " && ./out",
-                c = 'clear && gcc -o out "' .. file .. '" && ./out',
+				c = 'clear && gcc -o out "' .. file .. '" && ./out',
 			}
 
 			return ft_cmds[vim.bo.ft]
@@ -130,4 +130,4 @@ map({ "n", "t" }, "<leader>rc", function()
 end, { desc = "Run Current File" })
 
 -- Quick OO
-vim.api.nvim_set_keymap('n', 'OO', 'O<Esc>O', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "OO", "O<Esc>O", { noremap = true, silent = true })
