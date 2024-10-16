5f58c3ab602df5416c1a6a77f2b07d0dae04dc46
Pairadux <austincgause@gmail.com>
2024-10-16 02:54:59 -0400

    Config(nvim): added lazy.nvim to lazydev library

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -138,6 +138,7 @@ return {
 				-- See the configuration section for more details
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+                { path = "lazy.nvim", words = { "LazyVim" } }
 			},
 		},
 	},
