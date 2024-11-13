6cb976c2b2f56315152a3699574e57b16c6c845a
Pairadux <austincgause@gmail.com>
2024-11-13 12:00:57 -0500

    Chore: update todo comments

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,3 +1,4 @@
+-- TODO: add additional g key mappings for quick directory jumping
 require("full-border"):setup {
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
