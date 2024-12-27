efd401e55e5566769cb74581066f9630908b805d
Pairadux <austincgause@gmail.com>
2024-12-26 23:07:52 -0500

    Fix(nvim): adjust a variety of Obsidian settings

--- dot_config/nvim/lua/configs/obsidian.lua
+++ dot_config/nvim/lua/configs/obsidian.lua
@@ -1,21 +1,48 @@
 return {
 	workspaces = {
 		{
-			name = "SyncFolder",
-			path = "~/Documents/SyncFolder/",
+			name = "JDex",
+			path = "~/Documents/SyncFolder/00-09   System/00 System Management/00.00 JDex/",
+			overrides = {
+				notes_subdir = "",
+				templates = {
+					subdir = "00.05 Templates",
+					date_format = "%Y-%m-%d",
+					time_format = "%H:%M",
+					substitutions = {},
+				},
+			},
+		},
+		{
+			name = "Atomica",
+			path = "~/Documents/SyncFolder/90-99 󰝨  Atomica/",
+			overrides = {
+				notes_subdir = "",
+				templates = {
+					subdir = "templates",
+					date_format = "%Y-%m-%d",
+					time_format = "%H:%M",
+					substitutions = {},
+				},
+				daily_notes = {
+					folder = "daily/",
+					date_format = "%Y-%m-%d",
+					default_tags = { "daily" },
+					template = "daily-note-template",
+				},
+			},
 		},
 	},
 
-	notes_subdir = "90-99 󰝨  Atomica",
+	notes_subdir = "",
 
-	-- daily_notes = {
-	-- 	folder = "",
-	-- 	date_format = "%Y-%m-%d",
-	-- 	default_tags = { "daily-note" },
-	-- 	template = "00-09 System/05 Templates/daily-note-template.md",
-	-- },
+	completion = {
+		nvim_cmp = true,
+		min_chars = 2,
+	},
 
-	new_notes_location = "current_dir",
+	new_notes_location = "notes_subdir",
+
 	mappings = {
 		["gf"] = {
 			action = function()
@@ -146,40 +173,33 @@ return {
 		end
 	end,
 
+	wiki_link_func = "use_alias_only",
+
 	preferred_link_style = "wiki",
-	disable_frontmatter = false,
+	disable_frontmatter = true,
 
-	---@return table
-	note_frontmatter_func = function(note)
-		local jdIndex
-
-		if note.path then
-			local path_str = tostring(note.path)
-			local filename = path_str:match("([^/]+)%.md$")
-			if filename then
-				jdIndex = tonumber(filename:match("^%d%d%.%d%d"))
-			end
-		end
-
-		jdIndex = jdIndex or 0
-
-		local out = { tags = note.tags, jdIndex = jdIndex }
-
-		if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-			for k, v in pairs(note.metadata) do
-				out[k] = v
-			end
-		end
-
-		return out
-	end,
-
-	templates = {
-		subdir = "00-09   System/00 System Management/00.05 Templates",
-
-		date_format = "%Y-%m-%d",
-		time_format = "%H:%M",
-		-- A map for custom variables, the key should be the variable and the value a function
-		substitutions = {},
-	},
+	-- ---@return table
+	-- note_frontmatter_func = function(note)
+	-- 	local jdIndex
+	--
+	-- 	if note.path then
+	-- 		local path_str = tostring(note.path)
+	-- 		local filename = path_str:match("([^/]+)%.md$")
+	-- 		if filename then
+	-- 			jdIndex = tonumber(filename:match("^%d%d%.%d%d"))
+	-- 		end
+	-- 	end
+	--
+	-- 	jdIndex = jdIndex or 0
+	--
+	-- 	local out = { tags = note.tags, jdIndex = jdIndex }
+	--
+	-- 	if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+	-- 		for k, v in pairs(note.metadata) do
+	-- 			out[k] = v
+	-- 		end
+	-- 	end
+	--
+	-- 	return out
+	-- end,
 }
