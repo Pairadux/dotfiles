ab19aca04b912b7c5492fc877f02493b41366742
Pairadux <austincgause@gmail.com>
2025-01-31 00:18:49 -0500

    Feat(hyprland): add a floating neovide notepad

---
+++ dot_config/hypr/scripts/executable_notetaker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+noteFilename="$HOME/MEGA/00-09 System/00 System Management/00.00 JDex/00.07 Linux Quick Notes/$(date +%F).md"
+
+if [ ! -f "$noteFilename" ]; then
+    echo "## Notes for $(date +%F)" > "$noteFilename"
+fi
+
+neovide -- \
+    -c "norm Go" \
+    -c "norm Go#### $(date +%T)" \
+    -c "norm G2o" \
+    -c "norm zz" \
+    -c "startinsert" \
+    "$noteFilename"
