5fa28287f4f3b86fe62ff0695fc78964192fa6e3
Pairadux <austincgause@gmail.com>
2025-02-10 20:16:27 -0500

    Revert "Fix(hyprland): update notetaker script"
    This reverts commit 3b1838fc86166c705f252883011f95ee04515aac.

--- dot_config/hypr/scripts/executable_notetaker.sh
+++ dot_config/hypr/scripts/executable_notetaker.sh
@@ -13,8 +13,13 @@ if [ ! -f "$FILE" ]; then
         -e "s/<% tp.date.now(\"YYYY-MM-DD\", -1) %>/$YESTERDAY/g" \
         -e "s/<% tp.date.now(\"YYYY-MM-DD\", 1) %>/$TOMORROW/g" \
         "$TEMPLATE" > "$FILE"
+    echo -e "\n## Linux Quick Notes" >> "$FILE"
 fi
 
 NEOVIDE=1 neovide -- \
-    -c "norm Go | norm Go#### $(date +%T) | norm G2o | norm zz | startinsert" \ 
+    -c "norm Go" \
+    -c "norm Go#### $(date +%T)" \
+    -c "norm G2o" \
+    -c "norm zz" \
+    -c "startinsert" \
     "$FILE"
