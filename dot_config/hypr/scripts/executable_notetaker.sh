6781329eaa2fdfafc1b4ef222f083e619af62b84
Pairadux <austincgause@gmail.com>
2025-03-21 14:20:07 -0400

    Fix(hypr:notetaker): fix script

--- dot_config/hypr/scripts/executable_notetaker.sh
+++ dot_config/hypr/scripts/executable_notetaker.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 TODAY=$(date +"%F")
-YESTERDAY=$(date -d "yesterday" +"%F")
-TOMORROW=$(date -d "tomorrow" +"%F")
 
-TEMPLATE="$HOME/MEGA/00-09 System/00 System Management/00.00 JDex/00.05 Templates/dailySnippet.md"
+TEMPLATE="$HOME/MEGA/00-09 System/00 System Management/00.00 JDex/00.05 Templates/linuxQuickNoteSnippet.md"
 
-FILE="$HOME/MEGA/00-09 System/00 System Management/00.00 JDex/00.06 Daily Notes/$(date +%F).md"
+FILE="$HOME/MEGA/00-09 System/00 System Management/00.00 JDex/00.07 Linux Quick Notes/$TODAY.md"
 
 if [ ! -f "$FILE" ]; then
-    sed -e "s/<% tp.date.now(\"YYYY-MM-DD\") %>/$TODAY/g" \
-        -e "s/<% tp.date.now(\"YYYY-MM-DD\", -1) %>/$YESTERDAY/g" \
-        -e "s/<% tp.date.now(\"YYYY-MM-DD\", 1) %>/$TOMORROW/g" \
-        "$TEMPLATE" > "$FILE"
+    cat "$TEMPLATE" > "$FILE"
 fi
 
 NEOVIDE=1 neovide -- \
