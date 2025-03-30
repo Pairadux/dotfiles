f0dc0e3e8650dc37b85a6ba6d6537c55f8cb0a73
Pairadux <austincgause@gmail.com>
2025-03-30 19:42:21 -0400

    Style(nvim): align mappings and options
    Just prefer the look of the aligned elements

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,34 +1,25 @@
 local map = vim.keymap.set
 
+-- stylua: ignore start
+
 ------------------------------------------------------------
--- CORE
+-- INSERT MODE MAPPINGS
 ------------------------------------------------------------
 
--- Exit insert mode
-map('i', 'jk', '<ESC>')
+-- Exit Insert Mode
+map('i', 'jk', '<ESC>', { desc = 'Exit Insert Mode' })
 
--- Adjusted default zt behavior
--- map({ 'n', 'v' }, 'zt', 'zt2<C-y>', { noremap = true, silent = true })
-
--- Adjusted default C-d & C-u behavior
--- map("n", "<C-d>", "Lzz", { noremap = true, silent = true })
--- map("n", "<C-u>", "Hzz", { noremap = true, silent = true })
-
--- Quicker navigation
--- map({ 'n', 'v' }, '<C-j>', '5j', { noremap = true, silent = true })
--- map({ 'n', 'v' }, '<C-k>', '5k', { noremap = true, silent = true })
-
--- close nvim easier
--- map('n', '<C-q>', 'ZZ', { noremap = true, silent = true })
--- map("n", "<leader>qa", "<cmd>wqa<CR>", { desc = "Quit all" })
-
--- Insert Mode Mappings
-map('i', '<C-b>', '<ESC>^i', { desc = 'Move Beginning of Line' })
-map('i', '<C-e>', '<End>', { desc = 'Move End of Line' })
-map('i', '<C-h>', '<Left>', { desc = 'Move Left' })
+-- Navigation in Insert Mode
+map('i', '<C-b>', '<ESC>^i', { desc = 'Move to Beginning of Line' })
+map('i', '<C-e>', '<End>',   { desc = 'Move to End of Line' })
+map('i', '<C-h>', '<Left>',  { desc = 'Move Left' })
 map('i', '<C-l>', '<Right>', { desc = 'Move Right' })
-map('i', '<C-j>', '<Down>', { desc = 'Move Down' })
-map('i', '<C-k>', '<Up>', { desc = 'Move Up' })
+map('i', '<C-j>', '<Down>',  { desc = 'Move Down' })
+map('i', '<C-k>', '<Up>',    { desc = 'Move Up' })
+
+------------------------------------------------------------
+-- NORMAL MODE MAPPINGS
+------------------------------------------------------------
 
 -- Window Movement
 map('n', '<C-h>', '<C-w>h', { desc = 'Switch Window Left' })
@@ -37,42 +28,44 @@ map('n', '<C-j>', '<C-w>j', { desc = 'Switch Window Down' })
 map('n', '<C-k>', '<C-w>k', { desc = 'Switch Window Up' })
 
 -- Tab Management
-map('n', '<leader><Tab><Tab>', '<Cmd>tabnew<CR>', { desc = '[Tab] New [Tab]' })
-map('n', '<leader><Tab>n', 'gt', { desc = '[Tab] [N]ext' })
-map('n', '<leader><Tab>p', 'gT', { desc = '[Tab] [P]rev' })
+map('n', '<leader><Tab><Tab>', '<Cmd>tabnew<CR>', { desc = '[Tab] New Tab' })
+map('n', '<leader><Tab>n',     'gt',              { desc = '[Tab] Next' })
+map('n', '<leader><Tab>p',     'gT',              { desc = '[Tab] Prev' })
 
--- Clear Highlights
-map('n', '<Esc>', '<cmd>noh<CR>', { desc = 'General Clear Highlights' })
+-- Miscellaneous Normal Mappings
+map('n', '<Esc>',      '<cmd>noh<CR>',            { desc = 'Clear Highlights' })
+map('n', '<C-s>',      '<cmd>w<CR>',              { desc = 'Save File' })
+map('n', '<C-c>',      '<cmd>%y+<CR>',            { desc = 'Copy Whole File' })
+map('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'LSP Diagnostic Loclist' })
 
--- General Bindings
-map('n', '<C-s>', '<cmd>w<CR>', { desc = 'General Save File' })
-map('n', '<C-c>', '<cmd>%y+<CR>', { desc = 'General Copy Whole File' })
+-- Lazy Plugin
+map('n', '<leader>lo', '<cmd>Lazy<CR>',        { desc = '[Lazy] Open', noremap = true, silent = true })
+map('n', '<leader>lu', '<cmd>Lazy update<CR>', { desc = '[Lazy] Update', noremap = true, silent = true })
 
--- Global lsp mappings
-map('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'LSP [D]iagnostic [L]oclist' })
+-- Mason Plugin
+map('n', '<leader>mo', '<cmd>Mason<CR>', { desc = '[Mason] Open', noremap = true, silent = true })
 
--- Comment
--- map("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
--- map("v", "<leader>/", "gc", { desc = "Toggle Comment", remap = true })
-
--- Terminal
-map('t', '<C-x>', '<C-\\><C-N>', { desc = 'Terminal Escape Terminal Mode' })
-
--- Insert Line Break Sequence
-map('n', '<leader>ib', function()
-    require('util').insert_line_break()
-end, { noremap = true, silent = true, desc = "[I]nsert 80 #'s (Line [B]reak)" })
-
--- Insert Config Section Title Box
-map('n', '<leader>it', function()
-    require('util').insert_title_box()
-end, { noremap = true, silent = true, desc = '[I]nsert [T]itle Box' })
+-- Custom Utility Functions
+map('n', '<leader>ib', function() require('util').insert_line_break() end, { noremap = true, silent = true, desc = "Insert 80 #'s (Line Break)" })
+map('n', '<leader>it', function() require('util').insert_title_box() end, { noremap = true, silent = true, desc = 'Insert Title Box' })
 
 ------------------------------------------------------------
--- PLUGIN
+-- TERMINAL MODE MAPPINGS
 ------------------------------------------------------------
 
--- NvChad Code Runner {{{
+map('t', '<C-x>', '<C-\\><C-N>', { desc = 'Exit Terminal Mode' })
+
+------------------------------------------------------------
+-- ARCHIVED/DEPRECATED MAPPINGS
+------------------------------------------------------------
+
+-- map({ 'n', 'v' }, 'zt', 'zt2<C-y>', { noremap = true, silent = true })
+-- map("n", "<C-d>", "Lzz", { noremap = true, silent = true })
+-- map("n", "<C-u>", "Hzz", { noremap = true, silent = true })
+-- map({ 'n', 'v' }, '<C-j>', '5j', { noremap = true, silent = true })
+-- map({ 'n', 'v' }, '<C-k>', '5k', { noremap = true, silent = true })
+-- map('n', '<C-q>', 'ZZ', { noremap = true, silent = true })
+--- NvChad Code Runner {{{
 -- -- Code Runner
 -- map("n", "<leader>rc", function()
 -- 	vim.schedule(function()
@@ -96,11 +89,6 @@ end, { noremap = true, silent = true, desc = '[I]nsert [T]itle Box' })
 -- 			end,
 -- 		})
 -- 	end)
--- end, { desc = "[R]un [C]urrent File", noremap = true, silent = true })}}}
+-- end, { desc = "[R]un [C]urrent File", noremap = true, silent = true })}}}- map("n", "<leader>qa", "<cmd>wqa<CR>", { desc = "Quit all" })
 
--- Lazy Update
-map('n', '<leader>lo', '<cmd>Lazy<CR>', { desc = '[L]azy [U]pdate', noremap = true, silent = true })
-map('n', '<leader>lu', '<cmd>Lazy update<CR>', { desc = '[L]azy [U]pdate', noremap = true, silent = true })
-
--- Mason Open
-map('n', '<leader>mo', '<cmd>Mason<CR>', { desc = '[M]ason [O]pen', noremap = true, silent = true })
+-- stylua: ignore end
--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -3,36 +3,35 @@ local opt = vim.opt
 local o = vim.o
 local g = vim.g
 
+-- stylua: ignore start
 -----------------------------------------------------------
 -- Global Variables & Providers
 -----------------------------------------------------------
 g.tmux_navigator_no_mappings = 1
 
 -- Disable unused language providers
-g.loaded_node_provider = 0
+g.loaded_node_provider    = 0
 g.loaded_python3_provider = 0
-g.loaded_perl_provider = 0
-g.loaded_ruby_provider = 0
+g.loaded_perl_provider    = 0
+g.loaded_ruby_provider    = 0
 
 -----------------------------------------------------------
 -- Basic UI Settings
 -----------------------------------------------------------
-opt.number = true -- Absolute line numbers
-opt.relativenumber = true -- Relative line numbers
-opt.numberwidth = 2 -- Set width of number column
-opt.ruler = false -- Disable the ruler
-opt.mouse = "a" -- Enable mouse support
-opt.showmode = false -- Don't show mode (already in status line)
-opt.laststatus = 3 -- Use global statusline
-opt.scrolloff = 5 -- Minimal lines of context
+opt.number         = true  -- Absolute line numbers
+opt.relativenumber = true  -- Relative line numbers
+opt.numberwidth    = 2     -- Set width of number column
+opt.ruler          = false -- Disable the ruler
+opt.mouse          = "a"   -- Enable mouse support
+opt.showmode       = false -- Don't show mode (already in status line)
+opt.laststatus     = 3     -- Use global statusline
+opt.scrolloff      = 5     -- Minimal lines of context
 
 -----------------------------------------------------------
 -- Clipboard
 -----------------------------------------------------------
 -- Set clipboard after UIEnter to avoid startup delays
-vim.schedule(function()
-	opt.clipboard = "unnamedplus"
-end)
+vim.schedule(function() opt.clipboard = "unnamedplus" end)
 
 -----------------------------------------------------------
 -- Performance & Timings
@@ -50,7 +49,7 @@ opt.splitbelow = true -- Horizontal splits open below
 -- Search Settings
 -----------------------------------------------------------
 opt.ignorecase = true -- Case-insensitive searching...
-opt.smartcase = true -- ...unless uppercase is used
+opt.smartcase  = true -- ...unless uppercase is used
 
 -----------------------------------------------------------
 -- File Handling & Undo
@@ -60,29 +59,29 @@ opt.undofile = true -- Enable persistent undo
 -----------------------------------------------------------
 -- Indentation
 -----------------------------------------------------------
-opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.tabstop = 4 -- Tab Stop
-opt.shiftwidth = 4 -- Indent width
+opt.expandtab   = true -- Use spaces instead of tabs
+vim.opt.tabstop = 4    -- Tab Stop
+opt.shiftwidth  = 4    -- Indent width
 opt.smartindent = true -- Autoindent based on syntax
 
 -----------------------------------------------------------
 -- Visual & Display Options
 -----------------------------------------------------------
-opt.termguicolors = true -- Enable true colors for plugins like bufferline
-opt.cursorline = true -- Highlight current line
-opt.cursorlineopt = "both" -- Highlight line and number column
-opt.list = false -- Hide whitespace characters
-opt.fillchars = { eob = " " } -- End-of-buffer filler
-opt.inccommand = "split" -- Live preview for substitutions
-opt.wrap = false -- Disable line wrapping
-opt.whichwrap:append("<>[]hl") -- Allow cursor wrapping with these keys
-opt.spell = false
+opt.termguicolors = true          -- Enable true colors for plugins like bufferline
+opt.cursorline    = true          -- Highlight current line
+opt.cursorlineopt = "both"        -- Highlight line and number column
+opt.list          = false         -- Hide whitespace characters
+opt.fillchars     = { eob = " " } -- End-of-buffer filler
+opt.inccommand    = "split"       -- Live preview for substitutions
+opt.wrap          = false         -- Disable line wrapping
+opt.spell         = false         -- Disable spell checking
+opt.whichwrap:append("<>[]hl")    -- Allow cursor wrapping with these keys
 
 -----------------------------------------------------------
 -- Session & Folding Options (Global Options)
 -----------------------------------------------------------
 o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-o.foldmethod = "marker" -- Use markers for folding
+o.foldmethod     = "marker" -- Use markers for folding
 
 -----------------------------------------------------------
 -- Confirmation Dialogs
@@ -101,3 +100,4 @@ local is_windows = vim.fn.has("win32") ~= 0
 local sep = is_windows and "\\" or "/"
 local delim = is_windows and ";" or ":"
 vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH
+-- stylua: ignore end
