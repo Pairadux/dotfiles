a0c97cda801c7b0cc1ee664f19a4e63f35d21503
Pairadux <austincgause@gmail.com>
2025-04-07 09:28:39 -0400

    Style: format

--- dot_config/zsh/extras/executable_scripts.zsh
+++ dot_config/zsh/extras/executable_scripts.zsh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 
 function y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		builtin cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
 }
 
 vault_backup() {
