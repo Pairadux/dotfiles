b5c8b2ec1cdeb7c1a5089dc41ea11da551f26482
Pairadux <austincgause@gmail.com>
2025-04-20 09:57:27 -0400

    Fix(hyprland): add filetype to linux quick note

--- dot_config/hypr/scripts/executable_notetaker.sh
+++ dot_config/hypr/scripts/executable_notetaker.sh
@@ -4,7 +4,7 @@ TODAY=$(date +"%F")
 
 # TEMPLATE="$CLOUD/00-09 System/00 System Management/00.00 JDex/00.05 Templates/linuxQuickNoteSnippet.md"
 
-FILE="$CLOUD/10-19 Life Admin/15 Programming & Tech/15.07 Linux Quick Notes/$TODAY"
+FILE="$CLOUD/10-19 Life Admin/15 Programming & Tech/15.07 Linux Quick Notes/$TODAY.md"
 
 if [ ! -f "$FILE" ]; then
     printf "Notes for %s" "$TODAY" > "$FILE"
