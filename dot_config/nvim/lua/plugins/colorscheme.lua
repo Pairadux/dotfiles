396729d7fc1fdd186cc4651f2c102cd750735cf4
Pairadux <austincgause@gmail.com>
2025-04-29 11:11:28 -0400

    Config(nvim): make winseperator more visible

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -8,12 +8,32 @@
 return {
     {
         'folke/tokyonight.nvim',
-        priority = 1000, -- Make sure to load this before all the other start plugins.
+        priority = 1500, -- Make sure to load this before all the other start plugins.
         config = function()
             require('tokyonight').setup {
+                style = 'night',
+                transparent = false,
                 styles = {
-                    comments = { italic = false }, -- Disable italics in comments
+                    comments = { italic = true }, -- Disable italics in comments
+                    sidebars = 'dark', -- keeps sidebar bg darker
+                    floats = 'dark',
                 },
+                -- on_colors = function(colors)
+                --     -- you can tweak palette here if you want
+                --     -- e.g. colors.blue1 = "#89b4fa"
+                -- end,
+                on_highlights = function(highlights, colors)
+                    -- make the separator pop with one of the theme’s blues:
+                    highlights.WinSeparator = {
+                        fg = colors.blue1,
+                        bg = 'NONE',
+                    }
+                    -- fallback for older Neovim:
+                    highlights.VertSplit = {
+                        fg = colors.blue1,
+                        bg = 'NONE',
+                    }
+                end,
             }
             vim.cmd.colorscheme 'tokyonight-night'
         end,
