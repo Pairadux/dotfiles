ecf8ee480cbf58a17391db914b2543a1fd275857
Pairadux <austincgause@gmail.com>
2025-05-12 14:24:51 -0400

    Config(nvim): move config files to config dir

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -88,9 +88,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
-require 'options'
-require 'autocmds'
+require 'config.options'
+require 'config.autocmds'
 vim.schedule(function()
-    require 'mappings'
+    require 'config.mappings'
 end)
 require 'config.lazy'
