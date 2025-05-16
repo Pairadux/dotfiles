2a289f4bccd48c1fa4eae0a059796521fbb1de20
Pairadux <austincgause@gmail.com>
2025-05-16 15:02:46 -0400

    Fix(nvim): rename vim.highlight.on_yank to vim.hl.on_yank
    nvim-lua/kickstart.nvim@6ba2408cdf5eb7a0e4b62c7d6fab63b64dd720f6

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -98,6 +98,6 @@ autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
     callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end,
 })
