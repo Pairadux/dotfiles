0b2363417e77d04467987ecbb405ffea576f6b77
Pairadux <austincgause@gmail.com>
2025-05-29 14:21:22 -0400

    Config(nvim): have tabnew insert after last tabpage

--- dot_config/nvim/lua/config/mappings.lua
+++ dot_config/nvim/lua/config/mappings.lua
@@ -32,7 +32,7 @@ map('n', '<C-j>', '<C-w>j', { desc = 'Switch Window Down' })
 map('n', '<C-k>', '<C-w>k', { desc = 'Switch Window Up' })
 
 -- Tab Management
-map('n', '<leader><Tab>n', '<Cmd>tabnew<CR>  ', { desc = '[Tab] [N]ew Tab' })
+map('n', '<leader><Tab>n', '<Cmd>$tabnew<CR>  ', { desc = '[Tab] [N]ew Tab' })
 map('n', '<leader><Tab>x', '<Cmd>tabclose<CR>', { desc = '[Tab] Close'     })
 map('n', '<C-Tab>',        'gt',                { desc = '[Tab] Next'      })
 map('n', '<C-S-Tab>',      'gT',                { desc = '[Tab] Prev'      })
