1350f13b6e5f6be7022e94da390af9b649886fd3
Pairadux <austincgause@gmail.com>
2025-06-09 21:01:56 -0400

    Fix(hypr): update notetaker script with new paths

--- dot_config/hypr/scripts/executable_notetaker.sh
+++ dot_config/hypr/scripts/executable_notetaker.sh
@@ -2,19 +2,16 @@
 
 TODAY=$(date +"%F")
 
-# TEMPLATE="$CLOUD/00-09 System/00 System Management/00.00 JDex/00.05 Templates/linuxQuickNoteSnippet.md"
+TEMPLATE="$CLOUD/00-09 System/00 System Management/00.05 Templates/Daily-Note-Template.md"
 
-FILE="$CLOUD/10-19 Life Admin/15 Programming & Tech/15.07 Linux Quick Notes/$TODAY.md"
+FILE="$CLOUD/10-19 Life Admin/19 Atomica/19.06 Daily Notes/$TODAY.md"
 
 if [ ! -f "$FILE" ]; then
-    printf "Notes for %s" "$TODAY" > "$FILE"
-    # cat "$TEMPLATE" >> "$FILE"
+    cat "$TEMPLATE" >> "$FILE"
 fi
 
 NEOVIDE=1 neovide -- \
-    -c "norm Go" \
-    -c "norm Go#### $(date +%T)" \
-    -c "norm G2o" \
+    -c "norm Gkkk] jI- $(date +%H%M)-  " \
     -c "norm zz" \
     -c "startinsert" \
     "$FILE"
