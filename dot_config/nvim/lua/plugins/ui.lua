310203c3d33c316882e792deba690bda5b9b14db
Pairadux <austincgause@gmail.com>
2026-02-07 00:41:06 -0500

    config(nvim): remove git keybinds

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -202,30 +202,6 @@ return {
                         gitsigns.nav_hunk 'prev'
                     end
                 end, { desc = 'Jump to previous git [c]hange' })
-
-                -- Actions
-                -- visual mode
-                map('v', '<leader>gs', function()
-                    gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-                end, { desc = '[G]it [S]tage hunk' })
-                map('v', '<leader>gr', function()
-                    gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-                end, { desc = '[G]it [R]eset hunk' })
-                -- normal mode
-                map('n', '<leader>gs', gitsigns.stage_hunk, { desc = '[G]it [S]tage hunk' })
-                map('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[G]it [R]eset hunk' })
-                map('n', '<leader>gS', gitsigns.stage_buffer, { desc = '[G]it [S]tage buffer' })
-                map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = '[G]it [U]ndo stage hunk' })
-                map('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[G]it [R]eset buffer' })
-                map('n', '<leader>gp', gitsigns.preview_hunk, { desc = '[G]it [P]review hunk' })
-                map('n', '<leader>gb', gitsigns.blame_line, { desc = '[G]it [B]lame line' })
-                map('n', '<leader>gd', gitsigns.diffthis, { desc = '[G]it [D]iff against index' })
-                map('n', '<leader>gD', function()
-                    gitsigns.diffthis '@'
-                end, { desc = '[G]it [D]iff against last commit' })
-                -- Git Toggles
-                map('n', '<leader>gt', gitsigns.toggle_current_line_blame, { desc = '[G]it [T]oggle blame line' })
-                map('n', '<leader>gT', gitsigns.toggle_deleted, { desc = '[G]it [T]oggle deleted' })
             end,
         },
     }, -- }}}
