eaa60986c6df362a62938f16647bb3a7c9618737
Pairadux <dev@austingause.com>
2026-02-25 16:50:04 -0500

    config(nvim): configure neorg basics

--- dot_config/nvim/lua/plugins/extras/neorg.lua
+++ dot_config/nvim/lua/plugins/extras/neorg.lua
@@ -5,7 +5,26 @@ return {
     opts = {
         load = {
             ['core.defaults'] = {},
-            ['core.concealer'] = {},
+            ['core.concealer'] = {
+                config = {
+                    icon_preset = 'diamond',
+                    init_open_folds = 'always',
+                },
+            },
+            ['core.dirman'] = {
+                config = {
+                    default_workspace = "notes",
+                    workspaces = {
+                        notes = '~/Cloud/Notes/',
+                    },
+                },
+            },
+            ['core.summary'] = {},
+            ['core.journal'] = {
+                config = {
+                    workspace = 'notes'
+                },
+            },
         },
     },
 }
