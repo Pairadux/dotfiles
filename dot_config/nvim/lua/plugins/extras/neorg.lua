2a82b632d499db28d50e36e0c1cbfec29069fc26
Pairadux <dev@austingause.com>
2026-02-26 16:38:04 -0500

    fix(nvim): add completions for neorg

--- dot_config/nvim/lua/plugins/extras/neorg.lua
+++ dot_config/nvim/lua/plugins/extras/neorg.lua
@@ -1,30 +1,47 @@
 return {
-    'nvim-neorg/neorg',
-    lazy = false,
-    version = '*',
-    opts = {
-        load = {
-            ['core.defaults'] = {},
-            ['core.concealer'] = {
-                config = {
-                    icon_preset = 'diamond',
-                    init_open_folds = 'always',
-                },
-            },
-            ['core.dirman'] = {
-                config = {
-                    default_workspace = "notes",
-                    workspaces = {
-                        notes = '~/Cloud/Notes/',
+    {
+        'nvim-neorg/neorg',
+        lazy = false,
+        version = '*',
+        opts = {
+            load = {
+                ['core.defaults'] = {},
+                ['core.concealer'] = {
+                    config = {
+                        icon_preset = 'diamond',
+                        init_open_folds = 'always',
                     },
                 },
-            },
-            ['core.summary'] = {},
-            ['core.journal'] = {
-                config = {
-                    workspace = 'notes'
+                ['core.dirman'] = {
+                    config = {
+                        default_workspace = 'notes',
+                        workspaces = {
+                            notes = '~/Cloud/Notes/',
+                        },
+                    },
+                },
+                ['core.summary'] = {},
+                ['core.journal'] = {
+                    config = {
+                        workspace = 'notes',
+                    },
+                },
+                ['core.completion'] = {
+                    config = {
+                        engine = { module_name = 'external.lsp-completion' },
+                    },
+                },
+                ['external.interim-ls'] = {
+                    config = {
+                        completion_provider = {
+                            enable = true,
+                            documentation = true,
+                        },
+                    },
                 },
             },
         },
     },
+
+    { 'benlubas/neorg-interim-ls' },
 }
