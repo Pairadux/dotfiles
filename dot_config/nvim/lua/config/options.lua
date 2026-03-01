2dde2b597af0d28d6c6f5753ad1c891a521beb1e
Pairadux <dev@austingause.com>
2026-02-28 22:10:49 -0500

    feat(nvim): add option for conceal level

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -77,6 +77,7 @@ o.inccommand    = 'split'                                 -- Live preview for su
 o.wrap          = false                                   -- Disable line wrapping
 o.spell         = false                                   -- Disable spell checking
 o.list          = false                                   -- Hide whitespace characters
+o.conceallevel  = 3                                       -- Conceal things
 opt.listchars   = { tab = '» ', trail = '·', nbsp = '␣' } -- Whitespace characters (keep opt, not o)
 opt.fillchars:append({eob = '~' })                        -- End-of-buffer filler (keep opt, not o)
 opt.whichwrap:append('<>[]hl')                            -- Allow cursor wrapping with these keys (keep opt, not o)
