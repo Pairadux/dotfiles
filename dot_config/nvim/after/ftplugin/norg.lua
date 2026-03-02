e95b687f14b251b1bd15066d8aacc3cf2e46bc5c
Pairadux <dev@austingause.com>
2026-03-02 12:14:31 -0500

    config(nvim): add buffer specific options for norg files
    these options are mainly just to make writing better

--- dot_config/nvim/after/ftplugin/norg.lua
+++ dot_config/nvim/after/ftplugin/norg.lua
@@ -4,6 +4,19 @@ local map = vim.keymap.set
 -- Options
 vim.wo.foldlevel = 99
 
+-- Wrapping
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+vim.opt_local.textwidth = 0
+vim.opt_local.colorcolumn = ""
+
+-- Navigate display lines
+map("n", "j", "gj", { buffer = true, silent = true })
+map("n", "k", "gk", { buffer = true, silent = true })
+map("n", "0", "g0", { buffer = true, silent = true })
+map("n", "$", "g$", { buffer = true, silent = true })
+
 -- Which-key groups (buffer-local)
 require('which-key').add({
     { '<LocalLeader>t', group = '[T]ask',   buffer = true },
