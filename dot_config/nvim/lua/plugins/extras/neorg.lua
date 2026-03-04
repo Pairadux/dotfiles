bed9d5109564e60b4d4f39157df99f7faa921789
Pairadux <dev@austingause.com>
2026-03-04 18:45:24 -0500

    fix(nvim): update neorg build script to work cross-device

--- dot_config/nvim/lua/plugins/extras/neorg.lua
+++ dot_config/nvim/lua/plugins/extras/neorg.lua
@@ -7,18 +7,23 @@ return {
             local out = parser_dir .. '/norg.so'
             local tmp = vim.fn.tempname()
             vim.fn.mkdir(tmp, 'p')
+            local cc = vim.fn.executable 'gcc-15' == 1 and 'gcc-15' or 'cc'
+            local cxx = vim.fn.executable 'g++-15' == 1 and 'g++-15' or 'c++'
             local result = vim.fn.system(string.format(
                 [[
             git clone --depth 1 https://github.com/nvim-neorg/tree-sitter-norg %s 2>&1 &&
             cd %s &&
-            gcc-15 -c -std=c99 -fPIC -I./src -o %s/parser.o src/parser.c 2>&1 &&
-            g++-15 -c -std=c++14 -fPIC -I./src -o %s/scanner.o src/scanner.cc 2>&1 &&
-            g++-15 -shared -o %s %s/parser.o %s/scanner.o -lstdc++ 2>&1
+            %s -c -std=c99 -fPIC -I./src -o %s/parser.o src/parser.c 2>&1 &&
+            %s -c -std=c++14 -fPIC -I./src -o %s/scanner.o src/scanner.cc 2>&1 &&
+            %s -shared -o %s %s/parser.o %s/scanner.o -lstdc++ 2>&1
         ]],
                 tmp,
                 tmp,
+                cc,
                 tmp,
+                cxx,
                 tmp,
+                cxx,
                 out,
                 tmp,
                 tmp
