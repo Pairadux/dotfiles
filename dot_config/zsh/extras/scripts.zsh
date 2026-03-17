2459fdb7b987034eafbc19f90e21928a4574bc92
Pairadux <dev@austingause.com>
2026-03-17 19:07:51 -0400

    feat(zsh): add navi widget & binding

--- dot_config/zsh/extras/scripts.zsh
+++ dot_config/zsh/extras/scripts.zsh
@@ -108,6 +108,40 @@ ytdl() {
     yt-dlp "${opts[@]}" "$url"
 }
 
+_navi_call() {
+   local result="$(navi "$@" </dev/tty)"
+   printf "%s" "$result"
+}
+
+_navi_widget() {
+   local -r input="${LBUFFER}"
+   local -r last_command="$(echo "${input}" | navi fn widget::last_command)"
+   local replacement="$last_command"
+
+   if [ -z "$last_command" ]; then
+      replacement="$(_navi_call --print)"
+   elif [ "$LASTWIDGET" = "_navi_widget" ] && [ "$input" = "$previous_output" ]; then
+      replacement="$(_navi_call --print --query "$last_command")"
+   else
+      replacement="$(_navi_call --print --best-match --query "$last_command")"
+   fi
+
+   if [ -n "$replacement" ]; then
+      local -r find="${last_command}_NAVIEND"
+      previous_output="${input}_NAVIEND"
+      previous_output="${previous_output//$find/$replacement}"
+   else
+      previous_output="$input"
+   fi
+
+   zle kill-whole-line
+   LBUFFER="${previous_output}"
+   region_highlight=("P0 100 bold")
+   zle redisplay
+}
+
+zle -N _navi_widget
+
 # JUMP {{{
 # Easily jump around the file system by manually adding marks
 # marks are stored as symbolic links in the directory $MARKPATH (default $HOME/.marks)
