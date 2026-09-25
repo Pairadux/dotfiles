ece598e7a62c77d43f088af4ff4e9319ace7e58a
Pairadux <dev@austingause.com>
2026-09-25 10:24:20 -0400

    config(hypr): change spawn keybind to ctrl instead of alt

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -215,8 +215,8 @@ for _, n in ipairs(named) do
         desc("Named workspaces: Move window to " .. n.name))
 end
 
-hl.bind(mainMod .. " + ALT + T", hl.dsp.exec_cmd(terminal), desc("Spawn Terminal"))
-hl.bind(mainMod .. " + ALT + B", hl.dsp.exec_cmd(browser),  desc("Spawn Terminal"))
+hl.bind(mainMod .. " + CTRL + T", hl.dsp.exec_cmd(terminal), desc("Spawn Terminal"))
+hl.bind(mainMod .. " + CTRL + B", hl.dsp.exec_cmd(browser),  desc("Spawn Terminal"))
 
 -- Windows
 hl.bind(mainMod .. " + C",             hl.dsp.window.close(),                             desc("Windows: Close"))
